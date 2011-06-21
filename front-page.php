e0795e56036312f606e8f70e0b54b56214016ec0
David Valdez <me@gnuget.org>
2011-06-21 02:26:41 -0500

    first commit

---
+++ front-page.php
@@ -0,0 +1,3 @@
+<?php get_header();?>
+    <h1>hola</h1>
+<?php get_footer();?>
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html <?php language_attributes();?>>
+  <head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>" />
+    <title><?php wp_title(); ?> <?php bloginfo('name'); ?></title>
+    <link rel="profile" href="http://gmpg.org/xfn/11" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+    <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+    <?php if ( is_singular() && get_option( 'thread_comments' ) ) wp_enqueue_script( 'comment-reply' ); ?>  
+    <?php print wp_head();?>
+  </head>
+  <body>
+    <div id="content">
+      <div id="header">
+        <div class="menu">
+          <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Blog</li>
+            <li>Download</li>
+            <li>Support</li>
+          </ul>
+        </div> 
+      </div>
+    </div>
