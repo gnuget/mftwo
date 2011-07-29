f3a19d4213af88b4e84ce7e3980ea9080277a47c
hunk <ing.edgar@gmail.com>
2011-07-28 22:42:00 -0500

    change social links

--- front-page.php
+++ front-page.php
@@ -15,8 +15,23 @@
       <span>Some benefits of Magic Fields</span>
 
       <div class="social">
-        <a  class="twitter" href="http://twitter.com/magicfields" title="Follow us on twitter" alt="Follow us on twitter">&nbsp;</a>
-        <a  class="facebook" href="http://facebook.com" title="facebook" alt="facebook">&nbsp;</a>
+        <!-- <a  class="twitter" href="http://twitter.com/magicfields" title="Follow us on twitter" alt="Follow us on twitter">&nbsp;</a>
+        <a  class="facebook" href="http://facebook.com" title="facebook" alt="facebook">&nbsp;</a> -->
+
+        <a href="http://twitter.com/share" class="twitter-share-button" data-count="none" data-via="magicfields">Tweet</a><script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+
+        <!-- Place this tag where you want the +1 button to render -->
+        <g:plusone count="false"></g:plusone>
+
+        <!-- Place this tag after the last plusone tag -->
+        <script type="text/javascript">
+          (function() {
+              var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
+              po.src = 'https://apis.google.com/js/plusone.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
+          })();
+        </script>
+
         <div class="mf-clear"></div>
       </div>
     </div> 
